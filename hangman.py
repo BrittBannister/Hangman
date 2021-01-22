7db1e0beff0a9486fc3d1259289473199e46c0ee
Britt <brittbannister1127@icloud.com>
2021-01-22 12:36:02 -0500

    added json list of words to choose from, defined a function for the computer to guess a valid word, excluding dashes and spaces.

--- hangman.py
+++ hangman.py
@@ -1,1 +1,13 @@
-#pick a random english word. 
+import random
+from words import words
+
+
+def get_valid_word(words):
+    word = random.choice(words)
+    # print(word)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word.upper()
+
+
